3f76bfe6654d3877885b6e0d69f6418c129ff0df
Tom Wilson <twilson63@gmail.com>
2021-06-01 11:40:18 +0000

    update index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,5 @@
 <main class="mx-4 my-4 overflow-x-hidden">
   <h1 class="text-green-600 text-6xl">Welcome</h1>
-  <p>
-    Visit <a class="text-blue-600" href="https://kit.svelte.dev"
-      >kit.svelte.dev</a
-    > to read the documentation
-  </p>
   <section class="mt-4">
     <h3>Pages</h3>
     <ul class="list-disc ml-8">
