cf9dc157cb8662b0d8df337456098e7ba1c02b8b
tripott <tripott@gmail.com>
2021-06-09 22:29:12 +0000

    console log

--- src/routes/api/movies/search.json.js
+++ src/routes/api/movies/search.json.js
@@ -6,18 +6,13 @@ import fetch from 'node-fetch'
 const url = 'https://3000-salmon-antlion-fwwak7ju.ws-us08.gitpod.io/api/movies/_search'
 
 export async function post({ body }) {
-    console.log('search.json.js post', {body})
-    
     const result = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(body)
-
-      
-    }).then(r => {
-      console.log('routes/api/movies/search.json.js post() result', r)
+      body: JSON.stringify(body)  
+    }).then(r => { 
       return r.json()
     })
   
