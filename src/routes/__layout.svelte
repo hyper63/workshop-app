230c830b37bbe515792713d9b792a41c0e8c952c
tripott <tripott@gmail.com>
2021-06-18 14:13:19 +0000

    protecting review routes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,14 @@
+<script context="module">
+  export async function load({page, session}) {
+
+    console.log('page.path', page.path, 'session.username', session.username)
+    if (/^\/movies\/(.*)\/reviews\/(.*)/.test(page.path) && session.username === '') {
+      return { redirect: '/', status: 302 }
+    }
+    return { props: {} }
+  }
+</script>
+
 <script>
   import "../app.postcss";
 </script>
